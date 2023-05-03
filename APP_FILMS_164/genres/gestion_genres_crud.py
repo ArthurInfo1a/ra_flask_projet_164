ed5c42c7b19b17544ac2e06746e2ef6ee9826ec7
ArthurInfo1a <arthur.rey@eduvaud.ch>
2023-05-03 10:50:44 +0200

    n

--- APP_FILMS_164/genres/gestion_genres_crud.py
+++ APP_FILMS_164/genres/gestion_genres_crud.py
@@ -166,8 +166,8 @@ def genre_update_wtf():
                                           }
             print("valeur_update_dictionnaire ", valeur_update_dictionnaire)
 
-            str_sql_update_intitulegenre = """UPDATE t_personnes SET intitule_genre = %(value_name_genre)s, 
-            date_ins_genre = %(value_date_genre_essai)s WHERE id_personnes = %(value_id_genre)s """
+            str_sql_update_intitulegenre = """UPDATE t_personnes SET nom = %(value_name_genre)s, 
+            date_dajout = %(value_date_genre_essai)s WHERE id_personnes = %(value_id_genre)s """
             with DBconnection() as mconn_bd:
                 mconn_bd.execute(str_sql_update_intitulegenre, valeur_update_dictionnaire)
 
